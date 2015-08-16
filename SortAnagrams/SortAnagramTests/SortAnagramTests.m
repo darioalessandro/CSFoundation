416cac406bd4f25372bc5b78fc2f1076033f5868
Dario Talarico <darioalessandrolencina@gmail.com>
2015-08-15 23:07:50 -0400

    Missing files

--- SortAnagrams/SortAnagramTests/SortAnagramTests.m
+++ SortAnagrams/SortAnagramTests/SortAnagramTests.m
@@ -40,11 +40,4 @@
     XCTAssertEqualObjects(grouped, expected);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
